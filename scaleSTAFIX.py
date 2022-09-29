8a9c17aca972d3fe83ae51a6d52df7962410b22f
ppokor <pesbedrich0@gmail.com>
2022-09-29 18:51:46 +0200

    Added script

--- scaleSTAFIX.py
+++ scaleSTAFIX.py
@@ -1,9 +1,16 @@
 """
-# this is script for scaling amber topology file using STAFIX scheme. parmed is used for scaling. 
-# written by Pavlina Pokorna, 2022, nothing guaranteed.
-# do not use this for sclaing topologies that are to be converted gromacs! 
-do more elaborate what is this 
-requierements
+this is script to stafix-scale Amber topology with parmed tool of Ambertools
+requirements:
+	parmed and python 3 (remove input/output type definitions in is_to_be_scaled function to use this also with python 2)
+usage:
+	python scaleSTAFIX.py parm_file scaling_factor [mask]
+		[mask] is to be given in format resid1,resid2,resid3,... (e.g. 1,2,3,4). If no mask is specified, all RNA residues will be scaled
+flow:
+	run parmed printDetails for original topology file, then read the output and find atoms that are to be scaled (based on atomname and residuename) and get their IDs and LJ parameters
+	run parmed again this time with addLJType to make new type for atoms that we scale and then changeLJpair to scale epsilon for the selected atom pairs
+
+written by Pavlina Pokorna, 2022, nothing guaranteed.
+do not use this for scaling topologies that are to be converted to gromacs!
 """
 
 import os
@@ -27,7 +34,7 @@ TMPfile="TMPstafixParmed1.out"
 parmed_scale_input="TMPstafixParmed2.in"
 present_atom_types={}
 
-#This decides whether to scale the atom of a given name (atname) in a residue with name resname or not and returnt True or False
+#This decides whether to scale the atom of a given name (atname) in a residue with name resname or not and returns True or False
 def is_to_be_scaled(resname: str,atname: str) -> bool:
 	scaling=False
 	if resname in residue_namesA and atname not in atom_namesA+atom_namesSugar:
@@ -43,7 +50,8 @@ def is_to_be_scaled(resname: str,atname: str) -> bool:
 	return scaling
 
 # check if all arguments were given, if topology file exists, if scaling factor is a valid number, if mask is not provided in 1-5 format and if parmed is available. Exit if problem
-intro="\nthis is script to STAFIX-scale amber topology with parmed\nusage:\n\tpython scaleSTAFIX.py parm_file scaling_factor [mask]\n\n\t\t[mask] is to be given in format resid1,resid2,resid3,... (e.g. 1,2,3,4). If no mask is specified, all RNA residues will be scaled\n"
+intro="\nthis is script to STAFIX-scale amber topology with parmed\nusage:\n\tpython scaleSTAFIX.py parm_file scaling_factor [mask] \
+\n\n\t\t[mask] is to be given in format resid1,resid2,resid3,... (e.g. 1,2,3,4). If no mask is specified, all RNA residues will be scaled\n"
 try:
 	inputTOP=sys.argv[1]
 	if not os.path.exists(inputTOP):
@@ -63,17 +71,18 @@ except:
 	print(intro)
 	sys.exit()
 
-#filename not mane!!!!
-inputTOPfilemane,inputTOPextension=os.path.splitext(inputTOP)
-new_parm=inputTOPfilemane+"STAFIX"+str(scalefact)+inputTOPextension
+inputTOPfilename, inputTOPextension=os.path.splitext(inputTOP)
+new_parm= inputTOPfilename + "STAFIX" + str(scalefact) + inputTOPextension
 
 # remove parmed out file. if it exists, parmed does not override it 
 if os.path.exists(TMPfile):
 	os.remove(TMPfile) 
 
 #run parmed for the first time to get info about present atoms:
-os.system("parmed "+inputTOP+"<< EOF >> "+TMPfile+"\nprintDetails :"+mask+"\nEOF") 
-#check if procssed file was genereted or no!!!
+os.system("parmed "+inputTOP+"<< EOF >> "+TMPfile+"\nprintDetails :"+mask+"\nEOF")
+if not os.path.exists(TMPfile):
+	print("some parmed problem, exiting")
+	sys.exit()
 
 #load TMPstafixParmed1.out file and read info about atom types present and store it into dictionary present_atom_types. Dictionary format is: {atomtype : [atomids, LJradius, LJepsilon]}
 with open(TMPfile, "r") as TMPfile1:
@@ -103,15 +112,14 @@ with open(parmed_scale_input,"w") as parmedIn:
 		ljrad,ljdepth=present_atom_types[atom_type_group][1],present_atom_types[atom_type_group][2]
 		parmedIn.write("addLJType "+present_atom_types[atom_type_group][0]+" radius "+str(ljrad)+" epsilon "+str(ljdepth)+"\n")
 
-	#rename this shit!!!!!atom_properties_tuple forex PYCHARM
-	pat_tuple=list(present_atom_types.items())	#list of [atomids, LJradius, LJepsilon] for atomtypes
-	for i in range (0,len(pat_tuple)):
+	atom_properties_tuple=list(present_atom_types.items())	#list of [atomids, LJradius, LJepsilon] for atomtypes
+	for i in range (0, len(atom_properties_tuple)):
 		for j in range (0,i+1):
-			group1,group2=pat_tuple[i][1][0],pat_tuple[j][1][0]
-			eps1,eps2=pat_tuple[i][1][2],pat_tuple[j][1][2]
+			group1,group2= atom_properties_tuple[i][1][0], atom_properties_tuple[j][1][0]
+			eps1,eps2= atom_properties_tuple[i][1][2], atom_properties_tuple[j][1][2]
 			eps_orig=sqrt(eps1*eps2)
 			eps=str(eps_orig*scalefact)
-			r=str(pat_tuple[i][1][1]+pat_tuple[j][1][1])
+			r=str(atom_properties_tuple[i][1][1] + atom_properties_tuple[j][1][1])
 			parmedIn.write("changeLJPair "+group1+" "+group2+" "+r+" "+eps+"\n")
 
 	parmedIn.write("parmout "+new_parm+"\ngo")
